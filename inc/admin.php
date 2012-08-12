2e39cd8efaa161504c5fa119f44481cb317a55ab
Erik Bernskiold <erik@bernskioldmedia.com>
2012-08-12 18:32:26 +0200

    Admin Menu Separator
    + added admin menu separator
    
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- inc/admin.php
+++ inc/admin.php
@@ -22,6 +22,9 @@ class ISL_Admin extends Ilmenite_Slider {
 		// Runs the activation function
 		register_activation_hook( $this->_config->plugin_file, array( $this, 'activate' ) );
 		
+		// Registers admin menu separators
+		add_action( 'admin_menu', array( $this, 'admin_menu_separator' ) );
+		
 	}
 	
 	/**
@@ -33,6 +36,40 @@ class ISL_Admin extends Ilmenite_Slider {
 	 **/
 	public function activate() {
 		
+	}
+	
+	/**
+	 * Admin Menu Seprator
+	 *
+	 * @since Ilmenite Slider 1.0
+	 * @param integer
+	 **/
+	function add_admin_menu_separator($position) {
+
+		global $menu;
+		$index = 0;
+	
+		foreach($menu as $offset => $section) {
+			if (substr($section[2],0,9)=='separator')
+			    $index++;
+			if ($offset>=$position) {
+				$menu[$position] = array('','read',"separator{$index}",'','wp-menu-separator');
+				break;
+		    }
+		}
+	
+		ksort( $menu );
+	}
+	
+	/**
+	 * Adds admin menu separators
+	 *
+	 * @since Ilmenite Slider 1.0
+	 **/
+	function admin_menu_separator() {
+
+		// Adds custom separator after comments (at position 30)
+		$this->add_admin_menu_separator(30);
 	}
 	
 }
